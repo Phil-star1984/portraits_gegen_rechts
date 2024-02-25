4e7c21935f4d17098346280e52aa0808d5e03ee6
Philipp Mulfinger <info@phil-splash.de>
2024-02-25 19:03:38 +0100

    Add all sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import viteLogo from "/vite.svg"; */
 import "./App.css";
 
 function App() {
+  const handleClick = () => {
+    window.open(`mailto:info@phil-splash.de?subject=Portraitzeichnung "Portraits gegen Rechts"&body=Hallo Phil Splash, 
+
+    ich möchte bei der Aktion "Portraits gegen Rechts" mitmachen und ein Portrait erstellen lassen von: (hier bitte Vor- und Nachnamen einfügen und 1 od. 2 Bilder der Person mitschicken).`);
+  };
+
   return (
     <>
       <header>
@@ -12,9 +18,9 @@ function App() {
         </div>
         <nav>
           <ul>
-            <li>
+            {/* <li>
               <a href="#aktion">Aktion</a>
-            </li>
+            </li> */}
             <li>
               <a href="#mitmachen">Mitmachen</a>
             </li>
@@ -27,9 +33,9 @@ function App() {
             <li>
               <a href="#kuenstler">Der Künstler</a>
             </li>
-            <li>
+            {/* <li>
               <a href="#impressionen">Impressionen</a>
-            </li>
+            </li> */}
             <li>
               <a href="#impressum">Impressum</a>
             </li>
@@ -52,42 +58,43 @@ function App() {
         <h2>Die Aktion „Portraits gegen Rechts“</h2>
         <p>
           Portraits gegen Rechts ist eine Kunstaktion, die sich aktiv gegen jede
-          Form von Fremdenfeindlichkeit und Rechtsextremismus wendet. Im Laufe
-          der Aktion sollen gezeichnete Portraits von Würzburger Bürgern
-          entstehen, die bei der Vernissage im Rathaus am 04.04.2024 ausgestellt
-          werden.
+          Form von Fremdenfeindlichkeit und Rechtsextremismus wendet. <br />
+          Für die Aktion sollen vom 01.03. - 05.04.2024 von den Einwohnern
+          Würzburgs Portraits entstehen, die bei der Aktion mitmachen möchten.
+          Die entstandenen Portraits werden bei einer Vernissage im Würzburger
+          Rathaus ab dem 04.04.2024 einen Monat lang ausgestellt als Zeichen des
+          Widerstands gegen Rechts. Nach der Vernissage können die Portraits
+          gekauft werden.
         </p>
       </section>
+
+      <section id="mitmachen">
+        <h2>Wie kann ich mitmachen?</h2>
+        <p>
+          Du kannst innerhalb von wenigen Sekunden teilnehmen. Schicke ein Foto
+          von der Person, die gezeichnet werden soll, sowie den Vor- und
+          Nachnamen an info@phil-splash.de. Daraufhin zeichnet der Künstler das
+          Portrait, und es wird am Tag der Vernissage im Rathaus Würzburg
+          ausgestellt. Die Teilnahme bei der Aktion ist kostenlos; wer möchte,
+          kann das Portrait auch direkt nach der Ausstellung kaufen. Starte
+          jetzt! Lass von dir oder einer geliebten Person ein Portrait
+          anfertigen und setze ein Symbol für die Vielfalt und gegen Rechts.
+        </p>
+        <button onClick={handleClick}>Portraitvorlage hier senden</button>
+      </section>
       <div className="split-section">
-        <section id="mitmachen" style={{ backgroundColor: "#e9f5db" }}>
-          <h2>Wie kann ich mitmachen?</h2>
-          <p>
-            Du kannst innerhalb von wenigen Sekunden teilnehmen. Schicke ein
-            Foto von der Person, die gezeichnet werden soll, sowie den Vor- und
-            Nachnamen an info@phil-splash.de. Daraufhin zeichnet der Künstler
-            das Portrait, und es wird am Tag der Vernissage im Rathaus Würzburg
-            ausgestellt. Die Teilnahme bei der Aktion ist kostenlos; wer möchte,
-            kann das Portrait auch direkt nach der Ausstellung kaufen. Starte
-            jetzt! Lass von dir oder einer geliebten Person ein Portrait
-            anfertigen und setze ein Symbol für die Vielfalt und gegen Rechts.
-          </p>
-        </section>
-        <section
-          id="portraits"
-          style={{
-            backgroundImage: `url('https://www.wuerzburgerleben.de/wp-content/uploads/sites/10/2020/01/TheMillionPainter_Paintings_pink-1067x800.jpg')`,
-          }}
-        >
+        <section id="portraits">
           <h2>Einzigartige Portraits mit Inhalt</h2>
           <p>
-            Das Einzigartige an den Portraits ist, dass sie vor dem Paragraph 1
-            des Deutschen Grundgesetzes als Hintergrund gezeichnet werden und
-            somit zu einem individuellen Symbol für Toleranz und Vielfalt
-            werden.
+            Das Einzigartige an den Portraits ist, dass sie auf Paragraph 1 des
+            Deutschen Grundgesetzes als Hintergrund gezeichnet werden und somit
+            zu einem individuellen Symbol für Toleranz und Vielfalt werden.
           </p>
         </section>
+        <div id="portraits-example"></div>
       </div>
-      <section id="vernissage" style={{ backgroundColor: "#ffe5d9" }}>
+      <section id="portraitvorschau"></section>
+      <section id="vernissage">
         <h2>Die Vernissage der Portraits</h2>
         <p>
           Die Portraits werden gesammelt im Foyer des Rathauses Würzburg
@@ -96,7 +103,7 @@ function App() {
           Öffnungszeiten des Rathauses zu besuchen (Mo-Fr zwischen 9-17 Uhr).
         </p>
       </section>
-      <section id="kuenstler" style={{ backgroundColor: "#d9e2ff" }}>
+      <section id="kuenstler">
         <h2>Der Künstler</h2>
         <p>
           Phil Splash ist Web Developer, Art Director und Künstler. 2015 bekam
@@ -109,17 +116,150 @@ function App() {
           Zeitung, BILD).
         </p>
       </section>
-      <section id="impressionen" style={{ backgroundColor: "#f0f0f0" }}>
-        <h2>Impressionen</h2>
-        <div className="gallery">
-          <img
-            src="https://www.wuerzburgerleben.de/wp-content/uploads/sites/10/2020/01/TheMillionPainter_Paintings_pink-1067x800.jpg"
-            alt="Impression"
-          />
+      <section id="impressionen">
+        <div className="impressionen-text">
+          <h1>Impressionen</h1>
+        </div>
+        <section id="impressionen-gallery" className="gallery">
+          <figure className="gallery__item gallery__item--1">
+            <img
+              src="https://res.cloudinary.com/hzwebux5t/image/upload/v1573164242/uvncyxayeu1fyek7btfy.jpg"
+              class="gallery__img"
+              alt="Image 1"
+            />
+          </figure>
+          <figure className="gallery__item gallery__item--2">
+            <img
+              src="https://www.wuerzburgerleben.de/wp-content/uploads/sites/10/2020/01/Phil-Splash_Zeichnung_Bar.jpg"
+              class="gallery__img"
+              alt="Image 2"
+            />
+          </figure>
+          <figure className="gallery__item gallery__item--3">
+            <img
+              src="https://res.cloudinary.com/hzwebux5t/image/upload/c_fill,dpr_2.0,f_auto,g_auto,h_360,w_500/v1635189404/fgejp6iayeqfhlsi7mu0.jpg"
+              class="gallery__img"
+              alt="Image 3"
+            />
+          </figure>
+          <figure className="gallery__item gallery__item--4">
+            <img
+              src="https://www.wuerzburgerleben.de/wp-content/uploads/sites/10/2020/01/Collage_Phil-Splash.jpg"
+              class="gallery__img"
+              alt="Image 4"
+            />
+          </figure>
+          <figure className="gallery__item gallery__item--5">
+            <img
+              src="https://www.wuerzburgerleben.de/wp-content/uploads/sites/10/2020/01/TheMillionPainter_Stachuspassagen_Muenchen.jpg"
+              class="gallery__img"
+              alt="Image 5"
+            />
+          </figure>
+          <figure className="gallery__item gallery__item--6">
+            <img
+              src="https://www.wuerzburgerleben.de/wp-content/uploads/sites/10/2020/01/Phil-Splash_Malen_U-Bahn_Muenchen.jpg"
+              class="gallery__img"
+              alt="Image 6"
+            />
+          </figure>
+        </section>
+      </section>
+      <section id="impressum">
+        <h1>Impressum</h1>
+        <div className="impressum_inner_container">
+          <h2>Angaben gemäß § 5 TMG</h2> Philipp Mulfinger Phil* – Art+Design
+          Wiesenweg 13 97084 Würzburg Ust.-Idnr: DE 288086405
+          <h2>Haftungsausschluss</h2>
+          Haftung für Inhalte Die Inhalte unserer Seiten wurden mit größter
+          Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität
+          der Inhalte können wir jedoch keine Gewähr übernehmen. Als
+          Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
+          diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
+          bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+          übermittelte oder gespeicherte fremde Informationen zu überwachen oder
+          nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
+          hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung
+          von Informationen nach den allgemeinen Gesetzen bleiben hiervon
+          unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
+          Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei
+          Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+          Inhalte umgehend entfernen.
+          <h2>Haftung für Links</h2>
+          Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren
+          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
+          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine
+          permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne
+          konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+          umgehend entfernen.
+          <h2>Urheberrecht</h2>
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+          Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+          Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+          Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+          jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+          sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
+          wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
+          Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf
+          eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+          entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+          werden wir derartige Inhalte umgehend entfernen. Quellen: Disclaimer
+          von eRecht24, dem Portal zum Internetrecht von Rechtsanwalt Sören
+          Siebert
+          <h2>Datenschutzerklärung</h2>
+          Datenschutz: Die Nutzung unserer Webseite ist in der Regel ohne Angabe
+          personenbezogener Daten möglich. Soweit auf unseren Seiten
+          personenbezogene Daten (beispielsweise Name, Anschrift oder
+          eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets
+          auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche
+          Zustimmung nicht an Dritte weitergegeben. Wir weisen darauf hin, dass
+          die Datenübertragung im Internet (z.B. bei der Kommunikation per
+          E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der
+          Daten vor dem Zugriff durch Dritte ist nicht möglich. Der Nutzung von
+          im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch
+          Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung
+          und Informationsmaterialien wird hiermit ausdrücklich widersprochen.
+          Die Betreiber der Seiten behalten sich ausdrücklich rechtliche
+          Schritte im Falle der unverlangten Zusendung von Werbeinformationen,
+          etwa durch Spam-Mails, vor.
+          <h2>
+            Datenschutzerklärung für die Nutzung von Facebook-Plugins
+            (Like-Button)
+          </h2>
+          Auf unseren Seiten sind Plugins des sozialen Netzwerks Facebook, 1601
+          South California Avenue, Palo Alto, CA 94304, USA integriert. Die
+          Facebook-Plugins erkennen Sie an dem Facebook-Logo oder dem
+          „Like-Button“ („Gefällt mir“) auf unserer Seite. Eine Übersicht über
+          die Facebook-Plugins finden Sie hier:
+          http://developers.facebook.com/docs/plugins/. Wenn Sie unsere Seiten
+          besuchen, wird über das Plugin eine direkte Verbindung zwischen Ihrem
+          Browser und dem Facebook-Server hergestellt. Facebook erhält dadurch
+          die Information, dass Sie mit Ihrer IP-Adresse unsere Seite besucht
+          haben. Wenn Sie den Facebook „Like-Button“ anklicken während Sie in
+          Ihrem Facebook-Account eingeloggt sind, können Sie die Inhalte unserer
+          Seiten auf Ihrem Facebook-Profil verlinken. Dadurch kann Facebook den
+          Besuch unserer Seiten Ihrem Benutzerkonto zuordnen. Wir weisen darauf
+          hin, dass wir als Anbieter der Seiten keine Kenntnis vom Inhalt der
+          übermittelten Daten sowie deren Nutzung durch Facebook erhalten.
+          Weitere Informationen hierzu finden Sie in der Datenschutzerklärung
+          von facebook unter http://de-de.facebook.com/policy.php Wenn Sie nicht
+          wünschen, dass Facebook den Besuch unserer Seiten Ihrem
+          Facebook-Nutzerkonto zuordnen kann, loggen Sie sich bitte aus Ihrem
+          Facebook-Benutzerkonto aus. Quellen: Datenschutzerklärung eRecht24,
+          Facebook-Disclaimer von eRecht24
         </div>
       </section>
       <footer>
-        <p>© 2024 Portraits gegen Rechts. Alle Rechte vorbehalten.</p>
+        <p>
+          © 2024 Millionpainter | Portraits gegen Rechts. Alle Rechte
+          vorbehalten.
+        </p>
       </footer>
     </>
   );
